9e8e6426686055c272a7a4e8026430634df903f1
cgab <2204444@slu.edu.ph>
2021-04-19 20:48:13 +0800

    add: comments for getEventsOfVolunteer method

--- src/DBConnector.java
+++ src/DBConnector.java
@@ -36,11 +36,14 @@ public class DBConnector {
     /**
      * Returns a list of events a volunteer is part of. The list of events returned may contain different different
      * values depending on the inputted value of parameter returnType.
-     * TODO: clean
+     *
      * Algorithm:
-     * 1. Get list of scheduled events the volunteer is part of. Include the event's information
-     * 2. Get role of volunteer for each scheduled event and assign them respectively.
-     * 3
+     * 1. Get list of scheduled events the volunteer is part of
+     * 2. For every scheduled event:
+     *  2.1 If the event in which the schedule is for does not yet exist within the list of events to be returned,
+     *      create an event object and add it into the list.
+     *  2.2 Create a scheduleEvent object and add it into its appropriate event
+     * 3. Return the list
      * @param returnType an inputted value of:
      *                   0 - will return events a volunteer is volunteering for but is not yet verified to join
      *                   1 - will return events a volunteer is already verified to join
@@ -78,30 +81,25 @@ public class DBConnector {
             }
             s.setInt(1, volunteer.getId());
             ResultSet rs = s.executeQuery();
+            // For ever tuple
             while (rs.next()) {
                 int eventID = rs.getInt(2);
                 if (!events.contains(new Event(eventID))) {
+                    // If event does not exist create the event
                     List<EventSchedule> es = new ArrayList<>();
                     events.add(new Event(rs.getInt(2), rs.getString(1), rs.getString(3), es));
                 }
+                // Create event schedule
                 EventSchedule es = new EventSchedule(rs.getTimestamp(4),
                         rs.getTimestamp(5), rs.getString(6),
                         rs.getInt(7), new Role(rs.getString(9)), rs.getInt(8));
+                // Add event schedule to list of schedules for appropriate event
                 for (Event e : events) {
                     if (e.equals(new Event(eventID))) {
                         e.addSchedule(es);
                         break;
                     }
                 }
-                System.out.println("ename - " + rs.getString("e_name"));
-                System.out.println("eid - " + rs.getInt(2));
-                System.out.println("edesc - " + rs.getString(3));
-                System.out.println("start - " + rs.getTimestamp(4));
-                System.out.println("end - " + rs.getTimestamp(5));
-                System.out.println("location - " + rs.getString(6));
-                System.out.println("limit - " + rs.getInt(7));
-                System.out.println("schedID - " + rs.getInt(8));
-                System.out.println("rname - " + rs.getString(9));
             }
         }catch (SQLException | IllegalArgumentException e) {
             e.printStackTrace();
@@ -109,7 +107,6 @@ public class DBConnector {
         return events;
     }
 
-//    private List<Event>
 
     public static void closeConnection() throws Exception {
         if (con != null) {
--- src/Main.java
+++ src/Main.java
@@ -11,10 +11,12 @@ public class Main {
             byte b = 123;
             DBConnector.setConnection();
             List<Event> events = DBConnector.getEventsOfVolunteer(
-                    new Volunteer(2201029, "hatdog", User.Type.VOLUNTEER, "hatdog", "hatdog", b, "hatdog", 2201017
-                    )
+                    new Volunteer(2201029, "hatdog", User.Type.VOLUNTEER, "hatdog", "hatdog", b, "hatdog", 2201017)
                     , 2);
-            System.out.println(events.size());
+            System.out.println("events: " + events.size());
+            for (Event e : events) {
+                System.out.println(e.getName() + " - schedules: " + e.getSchedule().size());
+            }
         }catch (Exception e) {
             e.printStackTrace();
         }
